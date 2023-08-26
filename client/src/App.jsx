652d6e305bfeec4bf0db1b2eb7d1076d0b892dc1
Sayan Snigdha Pal <sayanpalb@gmail.com>
2023-08-26 11:06:27 +0530

    few changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LandingPage from './components/pages/LandingPage'
+import LandingPage from './pages/LandingPage'
+
 
 function App() {
 

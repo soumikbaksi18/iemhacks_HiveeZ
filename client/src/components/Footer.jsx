043e7ff268c9ac70176aa359a52073beeebd95a9
soumikbaksi18 <baksisoumik@gmail.com>
2023-08-26 12:41:34 +0530

    Footer update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className='flex justify-center bg-green-900'>
+      Powered by HiveeZ 
     </div>
   )
 }
